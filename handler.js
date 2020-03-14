c61abf77034ca8fc4b160f15d21d4cdeeef20d1a
Gauri <gauri.kangai@outlook.com>
2020-03-14 08:13:14 +0000

    Experiment to fetch raw weather data as a JSON object

--- handler.js
+++ handler.js
@@ -1,21 +1,38 @@
 'use strict';
 
-// const cors = require('cors');
-// app.use(cors()); 
+const fetch = require('node-fetch');
+require('dotenv').config();
+const cors = require('cors');
+app.use(cors()); 
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-module.exports.weather = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `What's the weather and do I need to pack an umbrella?`,
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+// module.exports.weather = async event => {
+  // return {
+
+    const api_key: process.env.API_KEY
+    // const loc_id: process.env.LOC_ID
+    
+    app.get ('/weather', async (req, res) => {
+      const api_url = `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/3772?res=3hourly&key=${api_key}`;
+      const fetch_response = await fetch (api_url);
+      const json = await fetch_response.json();
+      response.json(json);
+    });
+
+    // statusCode: 200,
+    // body: JSON.stringify(
+    //   {
+    //     message: `What's the weather and do I need to pack an umbrella?`,
+    //     input: event,
+    //   },
+    //   null,
+    //   2
+    // ),
+
+  module.exports.weather = serverless(app);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
