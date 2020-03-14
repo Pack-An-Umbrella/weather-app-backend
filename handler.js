cb232164deb059f5aa72422f94b7d59ed59e3e67
Gauri <gauri.kangai@outlook.com>
2020-03-14 14:55:06 +0000

    Experimenting a different way of requesting a get

--- handler.js
+++ handler.js
@@ -3,34 +3,49 @@
 const fetch = require('node-fetch');
 require('dotenv').config();
 const cors = require('cors');
-app.use(cors()); 
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // module.exports.weather = async event => {
   // return {
 
-    const api_key: process.env.API_KEY
-    // const loc_id: process.env.LOC_ID
+    const api_key = process.env.API_KEY,
+    const loc_id =  process.env.LOC_ID
     
-    app.get ('/weather', async (req, res) => {
-      const api_url = `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/3772?res=3hourly&key=${api_key}`;
+    //One way
+    app.get ('/weather/loc_id', async (req, res) => {
+      const loc_id = request.params.id;
+      const api_url = `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${loc_id}?res=3hourly&key=${api_key}`;
       const fetch_response = await fetch (api_url);
       const json = await fetch_response.json();
       response.json(json);
     });
 
-    // statusCode: 200,
-    // body: JSON.stringify(
-    //   {
-    //     message: `What's the weather and do I need to pack an umbrella?`,
-    //     input: event,
-    //   },
-    //   null,
-    //   2
-    // ),
+    // Another way
+    // getWeather = async () => {
+    //   const met_api = await fetch(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/3772?res=3hourly&key=fefe3677-6419-4792-bdf8-95f77d36453f`);
+    //   const weatherData = met_api.json();
+    //   console.log(weatherData);
+    // }
+
+
+
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: `What's the weather and do I need to pack an umbrella?`,
+//         input: event,
+//       },
+//       null,
+//       2
+//     ),
+
+//   };
+
+// };
 
   module.exports.weather = serverless(app);
 
